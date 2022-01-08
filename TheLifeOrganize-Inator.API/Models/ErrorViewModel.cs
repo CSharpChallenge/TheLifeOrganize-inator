27382208a44b812301404ab17240d60d404c8a11
Juliano Augusto <j.augusto@hotmail.com.br>
2022-01-08 04:39:35 -0300

    Initial Code Commit

---
+++ TheLifeOrganize-Inator.API/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheLifeOrganize_Inator.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
